06ccb34d73753d53cacd834b3df240981137c828
Pascal Wengerter <pascal@wengerter.info>
2024-01-25 16:13:02 +0000

    Try fixing autoprefixer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   devtools: { enabled: process.env.NODE_ENV === "development" },
   modules: ["@nuxt/content", "@nuxt/image"],
+  postcss: {
+    plugins: {
+      autoprefixer: {},
+    },
+  },
   ssr: false,
 });
