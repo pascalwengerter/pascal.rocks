0cedeec2a16c624d3357056ee562cd1abab5c1eb
Pascal Wengerter <pascal@wengerter.info>
2024-09-01 18:43:37 +0900

    Fix images not showing up(?)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
+  compatibilityDate: "2024-09-31",
   devtools: { enabled: process.env.NODE_ENV === "development" },
+  image: {
+    provider: "ipx",
+    domains: ["pascal.rocks"],
+    dir: "public/",
+  },
   modules: ["@nuxt/content", "@nuxt/image"],
   postcss: {
     plugins: {
