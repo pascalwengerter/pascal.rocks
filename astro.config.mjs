60e0932013c33711d940d59c72b3c03c29aecc33
Pascal Wengerter <pascal@wengerter.info>
2026-02-19 20:48:37 +0100

    Add rehype for external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://pascal.rocks',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		rehypePlugins: [
+			[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+		],
+	},
 });
